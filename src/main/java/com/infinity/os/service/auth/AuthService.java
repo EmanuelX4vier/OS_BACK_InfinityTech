35c8e6e862b0c76a9affd50e9dabf10d1bbf8269
Emanuel <emanuelxssi@gmail.com>
2026-08-23 23:36:55 -0300

    limpeza

--- src/main/java/com/infinity/os/service/auth/AuthService.java
+++ src/main/java/com/infinity/os/service/auth/AuthService.java
@@ -17,8 +17,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Locale;
-
 @Service
 @RequiredArgsConstructor
 public class AuthService {
